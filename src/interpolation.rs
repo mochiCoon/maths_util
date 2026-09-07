308d9bff2488f9b39f313a83743b7f29549cffb8
mochiCoon <282451528+mochiCoon@users.noreply.github.com>
2026-09-06 21:33:53 -0400

    Update smoothstep to use the proper clamp function

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -1,4 +1,4 @@
-use crate::arithmetic::{max, min};
+use crate::arithmetic::{clamp};
 
 pub fn lerp(a: f32, b: f32, t: f32) -> f32 {
     a + (b - a) * t
@@ -13,6 +13,7 @@ pub fn remap(value: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -
 }
 
 pub fn smoothstep(in_min: f32, in_max: f32, value: f32) -> f32 {
-    let x: f32 = max(0.0, min(1.0, (value-in_min)/(in_max-in_min)));
+    //let x: f32 = max(0.0, min(1.0, (value-in_min)/(in_max-in_min)));
+    let x = clamp((value-in_min)/(in_max-in_min), 0.0, 1.0);
     x*x*(3.0 - 2.0*x)
 }
